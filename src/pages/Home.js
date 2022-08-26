b1308e4d71d99df1214be0f31019c107bed22ca4
Hwang-Junsu <hjs77728@gmail.com>
2022-08-26 18:18:53 +0900

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,7 @@
 import React from "react";
-import {useDispatch} from "react-redux/es/exports";
+import {useSelector} from "react-redux";
 import Layout from "../component/Layout";
-import {toDoSlice} from "../redux/modules/slice";
 const Home = () => {
-  const dispatch = useDispatch();
-  dispatch(toDoSlice.actions.ADD()); // redux test
-
   return <Layout></Layout>;
 };
 
--- src/redux/modules/slice.js
+++ src/redux/modules/slice.js
@@ -2,7 +2,15 @@ import {createSlice} from "@reduxjs/toolkit";
 
 export const toDoSlice = createSlice({
   name: "todo",
-  initialState: [],
+  initialState: [
+    {
+      id: 0,
+      titie: "테스트입니다.",
+      body: "테스트입니다요.",
+      writer: "준수",
+      isDone: "false",
+    },
+  ],
   reducers: {
     ADD: (state, action) => {
       console.log("hello");
@@ -12,6 +20,14 @@ export const toDoSlice = createSlice({
 
 export const commentSlice = createSlice({
   name: "comment",
-  initialState: [],
+  initialState: [
+    {
+      id: "temp",
+      todoId: 0,
+      writer: "다른사람",
+      body: "댓글입니다.",
+      date: Date.now(),
+    },
+  ],
   reducers: {},
 });
