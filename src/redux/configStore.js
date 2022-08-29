2bba3a06f47880df027e4e22e7519c1747916fcf
woongsnote <mjw92@kakao.com>
2022-08-29 21:03:16 +0900

    code fix

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -5,6 +5,7 @@ import todos from "./modules/todosSlice";
 
 const store = configureStore({
   reducer: { todo: todo, comment: commentSlice.reducer, todos },
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export default store;
--- src/redux/modules/todosSlice.js
+++ src/redux/modules/todosSlice.js
@@ -8,12 +8,26 @@ const initialState = {
   isSuccess: false,
 };
 
+//get todos
+export const __getTodosThunk = createAsyncThunk(
+  "todos/getTodos",
+  async (payload, thunkAPI) => {
+    try {
+      const { data } = await axios.get("http://localhost:3001/todos");
+      return thunkAPI.fulfillWithValue(data);
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e);
+    }
+  }
+);
+
+/** DESC: add new todo */
 export const __addTodoThunk = createAsyncThunk(
   "todos/addTodo",
   async (payload, thunkAPI) => {
     try {
       const { data } = await axios.post("http://localhost:3001/todos", payload);
-      console.log(data);
+
       return thunkAPI.fulfillWithValue(data);
     } catch (e) {
       return thunkAPI.rejectWithValue(e);
@@ -30,6 +44,23 @@ export const todosSlice = createSlice({
     },
   },
   extraReducers: {
+    //DESC: get Todos
+
+    [__getTodosThunk.pending]: (state) => {
+      state.isLoading = true; // DESC: 네트워크 요청이 시작되면 로딩 상태를 true로 변경!
+    },
+    [__getTodosThunk.fulfilled]: (state, action) => {
+      state.isSuccess = true;
+      state.isLoading = false; // DESC: 네트워크 요청이 끝났으니, 로딩 상태를 false로 변경!
+      state.todos = action.payload; //DESC: Store에 있는 todos에 새 todo를 추가합니다.
+    },
+    [__getTodosThunk.rejected]: (state, action) => {
+      state.isLoading = false; // DESC: 네트워크 요청이 끝났으니, 로딩 상태를 false로 변경!
+      state.error = action.payload; // DESC: catch된 error 객체를 state.error에 넣습니다.
+    },
+
+    //DESC: Add New Todo
+
     [__addTodoThunk.pending]: (state) => {
       state.isLoading = true; // DESC: 네트워크 요청이 시작되면 로딩 상태를 true로 변경!
     },
