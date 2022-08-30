51c14e31b13566f829b4605c37840a61eb4726e1
woongsnote <mjw92@kakao.com>
2022-08-30 20:36:15 +0900

    code fix

--- src/redux/modules/slice.js
+++ src/redux/modules/slice.js
@@ -1,74 +1,5 @@
-import {
-  createAsyncThunk,
-  createAsyncThunk,
-  createSlice,
-} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import client from "../../client";
-
-const initialState = {
-  todos: [],
-  isLoading: false,
-  error: null,
-};
-
-export const __getTodos = createAsyncThunk(
-  "todos/getTodos",
-  async (payload, thunkAPI) => {
-    try {
-      console.log(123);
-      const data = await axios.get("http://localhost:3001/todos");
-      return thunkAPI.fulfillWithValue(data.data);
-    } catch (error) {
-      console.log(321);
-      return thunkAPI.rejectWithValue(error);
-    }
-  }
-);
-
-// export const __deleteTodos = createAsyncThunk(
-//   "todos/deleteTodos",
-//   async (payload, thunkAPI) => {
-//     try {
-//       const data = await axios.delete(`http://localhost:3001/todos/${payload}`);
-//       return thunkAPI.fulfillWithValue(data.data);
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error);
-//     }
-//   }
-// );
-
-export const toDoSlice = createSlice({
-  name: "todos",
-  initialState,
-  reducers: {
-    addTodo: (state, action) => {
-      state.todos = [...state.todos, action.payload];
-    },
-    deleteTodo: (state, action) => {
-      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
-    },
-    editTodo: (state, action) => {
-      state.todos = state.todos.map((todo) =>
-        todo.id === action.payload.id
-          ? { ...todo, body: action.payload.body }
-          : todo
-      );
-    },
-  },
-  extraReducers: {
-    [__getTodos.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [__getTodos.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.todos = action.payload;
-    },
-    [__getTodos.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-  },
-});
 
 const initialState = {
   comments: [],
@@ -170,7 +101,6 @@ export const commentSlice = createSlice({
   },
 });
 
-export const { addTodo, deleteTodo, editTodo, loadTodo } = toDoSlice.actions;
 export const { addComment, deleteComment, updateComment } =
   commentSlice.actions;
-export default toDoSlice.reducer;
+export default commentSlice.reducer;
