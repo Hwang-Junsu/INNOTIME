1a185908fabb149503b510a4a84e8530c9bbc580
Hwang-Junsu <hjs77728@gmail.com>
2022-08-31 22:58:57 +0900

    완성

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import GlobalStyle from "./style/global";
 import "./style/font.css";
 
 function App() {
+  if (process.env.NODE_ENV === "production") {
+    console.log = function no_console() {};
+  }
+  console.log("확인용 콘솔입니다.");
   return (
     <>
       <GlobalStyle />
